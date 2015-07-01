c2fcfa7f0c04c9bffbcbe89d8ebf76b2d8671c3d
Ozzy Johnson <oswald.johnson@gsa.gov>
2015-07-01 19:36:08 -0400

    pass with nodelint

--- jobs/cf-quota-perm-set/cf-cron.js
+++ jobs/cf-quota-perm-set/cf-cron.js
@@ -1,30 +1,30 @@
 // Give a little help when failing.
-function friendlyExit(reason){
-  if (reason == 'env_job'){
+function friendlyExit(reason) {
+  if (reason === 'env_job') {
     console.log('You must set the environment variable CRON_JOB.');
     process.exit();
-  };
-};
+  }
+}
 
 // Does the parameter look like the name of a service credential.
 function credEval(cred) {
-  if (cred.split(".")[0] == 'creds'){
+  if (cred.split(".")[0] === 'creds') {
     return eval(cred);
-  } else {
-    return cred
-  };
-};
+  }
+  return cred;
+}
 
 // Clean up parameters and check if they're service credentials.
 function evalJob(job) {
-  job = job.split(comma),
-  job = job.map(Function.prototype.call, String.prototype.trim),
+  var comma = ',';
+  job = job.split(comma);
+  job = job.map(Function.prototype.call, String.prototype.trim);
   job = job.map(credEval);
-  return job
-};
+  return job;
+}
 
 // Get the name of our user-provided credential service.
-var cf_creds = process.env.CF_CREDS
+var cf_creds = process.env.CF_CREDS;
 
 // Use cfenv to grab our credentials from the credential service.
 var cfenv = require("cfenv");
@@ -32,7 +32,6 @@ var appEnv = cfenv.getAppEnv();
 var creds = appEnv.getServiceCreds(cf_creds);
 
 // Get the command.
-var comma = ',';
 var env_job = process.env.CRON_JOB || friendlyExit('env_job');
 
 // Clean up extra whitespace here to give some leeway in job formatting.
@@ -42,26 +41,26 @@ var prep_job = process.env.PREP_JOB || false;
 if (prep_job) {
   var prep = evalJob(prep_job);
 } else {
-  var prep = ":"
-};
+  var prep = ":";
+}
 
 // Parse the cron job.
 var job = evalJob(env_job);
 
 // Get the command schedule.
-var schedule = process.env.CRON_SCHEDULE
+var schedule = process.env.CRON_SCHEDULE;
 
 // Lets begin.
 console.log("Started...");
 
 // Run our prep commands.
-var spawn = require('child_process').spawn
+var spawn = require('child_process').spawn;
 
 if (prep.length > 1) {
   var prep_run = spawn(prep[0], prep.slice(1));
 } else {
   var prep_run = spawn(prep[0]);
-};
+}
 
 prep_run.stdout.on('data', function (data) {
   console.log('Prep_Out: ' + data);
@@ -76,27 +75,28 @@ prep_run.on('close', function (code) {
 
   // Set up cron and run the job.
   console.log('Scheduling...');
+
   var CronJob = require('cron').CronJob;
 
-  new CronJob(schedule, function() {
+  new CronJob(schedule, function () {
 
-      if (job.length > 1) {
-        var job_run = spawn(job[0], job.slice(1));
-      } else {
-        var job_run = spawn(job[0]);
-      };
+    var job_run = spawn(job[0], job.slice(1));
 
-      job_run.stdout.on('data', function (data) {
-        console.log('Job_Out: ' + data);
-      });
+    if (job.length > 1) {
+      job_run = spawn(job[0]);
+    }
 
-      job_run.stderr.on('data', function (data) {
-        console.log('Job_Err: ' + data);
-      });
+    job_run.stdout.on('data', function (data) {
+      console.log('Job_Out: ' + data);
+    });
 
-      job_run.on('close', function (code) {
-        console.log('Job_Exit:' + code);
-      });
+    job_run.stderr.on('data', function (data) {
+      console.log('Job_Err: ' + data);
+    });
+
+    job_run.on('close', function (code) {
+      console.log('Job_Exit:' + code);
+    });
   }, null, true, 'America/New_York');
 
 });
